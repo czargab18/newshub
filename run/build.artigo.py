89174745bcac8f8db258d868c746152e86e39e91
czargab18 <87340795+czargab18@users.noreply.github.com>
2025-08-25 13:21:06 -0300

    feat: remover importações não utilizadas e organizar a estrutura do código

--- run/build.artigo.py
+++ run/build.artigo.py
@@ -4,17 +4,6 @@ Script Python para renderização de Markdown - Estatística Newsroom
 Versão: 2.0 com pypandoc e processamento avançado de componentes
 """
 
-import os
-import sys
-import yaml
-import re
-import json
-import argparse
-import webbrowser
-from pathlib import Path
-from typing import Optional
-from datetime import datetime
-
 
 class Newshub:
   """Automação para renderização de Markdown - Estatística Newsroom"""
@@ -23,9 +12,21 @@ class Newshub:
   ## --input do artigo renderizado
   ## --output do artigo automatizado
   ## --template para o conteúdo do artigo renderizado
-  
-  ...
 
+  def __init__(self, input_file, output_file, template_file):
+    ...
+
+  # ler() : ler o conteúdo do artigo renderizado ou template HTML.
+  # artigo = ler()
+  # template = ler()
+
+  # extrair conteúdo artigo
+  # pegar o info <header>: .title, .subtitle, quarto-title-meta>div> .date
+  # title = fun pega title do artigo
+  # subtitle = fun pega subtitle do artigo
+  # date = fun pega date do artigo
+
+  # pegar o conteúdo do artigo
 class Newsroom:
   """Mover arquivos para a pasta correta e organizar diretórios"""
   ...
